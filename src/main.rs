1e98860763c58b16fd550d32a015e7abbe851ddf
lemmaandrew <lemmajandrew@gmail.com>
2023-11-28 18:28:25 -0600

    rewrote a bunch of stuff for optimization

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,33 @@
 use image::{open, ImageBuffer, Rgb, RgbImage};
 use rand::random;
 use std::env;
+use std::time::Instant;
 
 /// Draws a random single colored rectangle on the image at given coordinates.
+/// Returns the random color and the pixel values that were overwritten
 fn draw_rectangle(
     image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
     top_left: (u32, u32),
     bottom_right: (u32, u32),
-) {
+) -> (Vec<Vec<Rgb<u8>>>, Rgb<u8>) {
     let color = Rgb([random(), random(), random()]);
-    for y in top_left.1..bottom_right.1 {
-        for x in top_left.0..bottom_right.0 {
+    let mut original_pixels = Vec::new();
+    for x in top_left.0..bottom_right.0 {
+        let mut column = Vec::new();
+        for y in top_left.1..bottom_right.1 {
+            column.push(*image.get_pixel(x, y));
             image.put_pixel(x, y, color);
         }
+        original_pixels.push(column);
     }
+    (original_pixels, color)
 }
 
-/// Draw a rectangle on the given image at a random location with a random color
+/// Draw a rectangle on the given image at a random location with a random color.
+/// Returns the color and the top-left and bottom-right vertices of the rectangle
 fn get_neighbor(
-    mut image: ImageBuffer<Rgb<u8>, Vec<u8>>,
-) -> (ImageBuffer<Rgb<u8>, Vec<u8>>, ((u32, u32), (u32, u32))) {
+    image: &mut ImageBuffer<Rgb<u8>, Vec<u8>>,
+) -> ((Vec<Vec<Rgb<u8>>>, Rgb<u8>), ((u32, u32), (u32, u32))) {
     let (w, h) = image.dimensions();
     let bottom_right = (random::<u32>() % (w + 1), random::<u32>() % (h + 1));
     // if `bottom_right` contains any 0s, we must account for that
@@ -33,8 +41,8 @@ fn get_neighbor(
             random::<u32>() % bottom_right.1,
         ),
     };
-    draw_rectangle(&mut image, top_left, bottom_right);
-    (image, (top_left, bottom_right))
+    let (original_pixels, new_color) = draw_rectangle(image, top_left, bottom_right);
+    ((original_pixels, new_color), (top_left, bottom_right))
 }
 
 /// Difference between two pixels as a single value
@@ -66,14 +74,14 @@ fn get_cost(
 
 /// A less expensive version of `get_cost`.
 /// Takes a previous `get_cost` result, resets it to the sum of pixel differences,
-/// subtracts the pixel differences between the original image and the first generated image for a
-/// given area, adds back in the pixel differences between the original image and the second
-/// generated image for that area, then returns the total distance calculation.
+/// subtracts the pixel differences between the original image and the generated image for a
+/// given area, adds back in the pixel differences between the original image and the new color
+/// and then calculates the new distance result
 fn update_cost(
     previous_cost: f64,
     original_image: &ImageBuffer<Rgb<u8>, Vec<u8>>,
-    generated_image1: &ImageBuffer<Rgb<u8>, Vec<u8>>,
-    generated_image2: &ImageBuffer<Rgb<u8>, Vec<u8>>,
+    old_pixels: &Vec<Vec<Rgb<u8>>>,
+    new_color: Rgb<u8>,
     top_left: (u32, u32),
     bottom_right: (u32, u32),
 ) -> f64 {
@@ -82,21 +90,26 @@ fn update_cost(
     let mut s = (previous_cost * previous_cost * (w * h * 3) as f64)
         .sqrt()
         .round() as u64;
-    // subtracting off the relevant pixels from the first generated image
+    // subtracting off the relevant pixels from the first generated image.
+    // also storing `original_image`'s pixels so we don't have to fetch them again
+    // because apparently `get_pixel` is an expensive operation??
+    let mut original_pixels = Vec::new();
     for x in top_left.0..bottom_right.0 {
+        let mut column = Vec::new();
         for y in top_left.1..bottom_right.1 {
-            s -= pixel_difference(
-                *original_image.get_pixel(x, y),
-                *generated_image1.get_pixel(x, y),
-            );
+            let original_pixel = *original_image.get_pixel(x, y);
+            let old_pixel = old_pixels[(x - top_left.0) as usize][(y - top_left.1) as usize];
+            s -= pixel_difference(original_pixel, old_pixel);
+            column.push(original_pixel);
         }
+        original_pixels.push(column);
     }
     // adding in the relevant pixels from the second generated image
     for x in top_left.0..bottom_right.0 {
         for y in top_left.1..bottom_right.1 {
             s += pixel_difference(
-                *original_image.get_pixel(x, y),
-                *generated_image2.get_pixel(x, y),
+                original_pixels[(x - top_left.0) as usize][(y - top_left.1) as usize],
+                new_color,
             );
         }
     }
@@ -116,28 +129,45 @@ fn anneal(
     let mut image = RgbImage::new(original_image.dimensions().0, original_image.dimensions().1);
     let mut cost = get_cost(&original_image, &image);
 
+    let total_time_start = Instant::now();
     while current_temp >= final_temp {
-        print!("{current_temp}\r");
-        let (neighbor, (top_left, bottom_right)) = get_neighbor(image.clone());
+        let ((image_pixels, new_color), (top_left, bottom_right)) = get_neighbor(&mut image);
         let neighbor_cost = update_cost(
             cost,
             original_image,
-            &image,
-            &neighbor,
+            &image_pixels,
+            new_color,
             top_left,
             bottom_right,
         );
         let cost_diff = neighbor_cost - cost;
         if cost_diff < 0.0 {
-            image = neighbor;
             cost = neighbor_cost;
         } else if random::<f64>() < (-cost_diff / current_temp).exp() {
-            image = neighbor;
             cost = neighbor_cost;
+        } else {
+            // reset pixels to the old image pixels instead of neighbor pixels
+            for x in top_left.0..bottom_right.0 {
+                for y in top_left.1..bottom_right.1 {
+                    image.put_pixel(
+                        x,
+                        y,
+                        image_pixels[(x - top_left.0) as usize][(y - top_left.1) as usize],
+                    );
+                }
+            }
         }
         current_temp *= alpha;
+        print!(
+            "temperature: {current_temp}\r",
+        );
     }
 
+    let total_time_elapsed = total_time_start.elapsed();
+    println!(
+        "\ntotal time elapsed: {} seconds",
+        total_time_elapsed.as_secs_f64()
+    );
     image
 }
 
